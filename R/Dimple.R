bea3098e461728ae8d1af175993f074b0c72b611
timelyportfolio <kent.russell@timelyportfolio.com>
2013-10-23 14:25:23 -0500

    get newest Dimple.R for color palette spec

--- R/Dimple.R
+++ R/Dimple.R
@@ -7,10 +7,11 @@ dPlot <- dimplePlot <- function(x, data, ...){
 Dimple <- setRefClass('Dimple', contains = 'rCharts', methods = list(
   initialize = function(){
     callSuper(); 
-    params <<- c(params, list(
+    params <<- c(params, list(controls = list(),
       chart = list(), xAxis = list(type="addCategoryAxis", showPercent = FALSE),
       yAxis = list(type="addMeasureAxis", showPercent = FALSE),
-      zAxis = list(), colorAxis = list(), legend = list()
+      zAxis = list(), colorAxis = list(), defaultColors = list(),
+      legend = list(), facet = list()
     ))
   },
   chart = function(..., replace = F){
@@ -25,31 +26,46 @@ Dimple <- setRefClass('Dimple', contains = 'rCharts', methods = list(
   zAxis = function(..., replace = F){
     params$zAxis <<- setSpec(params$zAxis, ..., replace = replace)
   },
+  defaultColors = function(..., replace = F){
+    params$defaultColors <<- setSpec(params$defaultColors, ..., replace = replace)
+  },    
   colorAxis = function(...){
     .self$set(colorAxis = list(...))
   },
   legend = function(...){
     .self$set(legend = list(...))
   },
+  facet = function(...){
+    .self$set(facet = list(...))
+  },
   getChartParams = function(...){
     params <<- modifyList(params, getLayer(...))
   },
   getPayload = function(chartId){
+    #l = plyr::alply(params$data, 1, as.list)
+    #names(l) = NULL
+    #data = rjson::toJSON(l)
     data = toJSONArray(params$data)
     #there is potential to  chain the entire thing
     #making much cleaner
     #need to explore this
     #as of now thought chart is not being used
-    chart = toChain(params$chart, 'myChart')
-    #cannot eliminate so changed toChain to toJSON
+    chart = toChain3(params$chart, 'myChart')
+       #cannot eliminate so changed toChain to toJSON
     #but need to revert back to toChain for the axes
     xAxis = toJSON(params$xAxis) #toChain(params$xAxis, 'chart.xAxis')
     yAxis = toJSON(params$yAxis) #toChain(params$yAxis, 'chart.yAxis')
     zAxis = toJSON(params$zAxis)
     colorAxis = toJSON(params$colorAxis)
+    defaultColors = toJSON2(params$defaultColors)
+    controls_json = toJSON(params$controls)
+    controls = setNames(params$controls, NULL)     
     legend = toJSON(params$legend)
-    opts = toJSON(params[!(names(params) %in% c('data', 'chart', 'xAxis', 'yAxis', 'zAxis', 'colorAxis', 'legend'))])
-    list(opts = opts, xAxis = xAxis, yAxis = yAxis, zAxis = zAxis, colorAxis = colorAxis, legend = legend, data = data, 
-         chart = chart, chartId = chartId)
+    opts = toJSON(params[!(names(params) %in% c('data', 'chart', 'xAxis', 'yAxis', 'zAxis', 'colorAxis', 'defaultColors', 'legend','controls'))])
+    list(opts = opts, xAxis = xAxis, yAxis = yAxis, zAxis = zAxis,
+         colorAxis = colorAxis, defaultColors = defaultColors,
+         legend = legend, controls = controls, 
+         controls_json = controls_json, data = data, 
+         chart = chart, chartId = chartId, CODE = srccode)
   }
 ))
