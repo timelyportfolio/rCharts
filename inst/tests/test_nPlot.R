cc46ffe06904c60c452915117b1f19733f789e2c
timelyportfolio <kent.russell@timelyportfolio.com>
2014-01-28 19:56:29 -0600

    work through various nvd3 tests;sure these will need to be significantly refined

--- inst/tests/test_nPlot.R
+++ inst/tests/test_nPlot.R
@@ -1,5 +1,34 @@
 context("nPlot instantiation")
 
-test_that("nPlot() creates expected structure", {
+hairEye <- data.frame(HairEyeColor)
+
+test_that("nPlot creates expected structure", {
   expect_that(nPlot(), throws_error())
+  expect_that(
+    nPlot(
+      Freq ~ Hair,
+      group = "Sex",
+      data = subset(hairEye, Eye=="Brown"),
+      type = "multiBarChart"
+    ),
+    is_a("Nvd3")
+  )
+  expect_that(
+    nPlot(
+      Freq ~ Hair,
+      group = "Sex",
+      data = subset(hairEye, Eye=="Brown"),
+      type = "multiBarChart"
+    )$params,
+    is_a("list")
+  )
+  expect_identical(
+    nPlot(
+      Freq ~ Hair,
+      group = "Sex",
+      data = subset(hairEye, Eye=="Brown"),
+      type = "multiBarChart"
+    )$params$x,
+    "Hair"
+  )
 })
