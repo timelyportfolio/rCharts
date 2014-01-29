07bfb33c0d69d2eafa8facc55107f4bec397de93
timelyportfolio <kent.russell@timelyportfolio.com>
2014-01-28 20:50:17 -0600

    toyed with idea of using XML::htmlParse to test output

--- inst/tests/test_nPlot.R
+++ inst/tests/test_nPlot.R
@@ -25,4 +25,30 @@ test_that("nPlot creates expected structure", {
     n1$params$y,
     "Freq"
   )
+  expect_identical(
+    n1$params$data,
+    subset(hairEye,Eye=="Brown")
+  )
+})
+
+context("nPlot makes things as expected")
+
+test_that("nPlot output seems correct",{
+  expect_that(
+    n1$getPayload(chartId = n1$params$id),
+    is_a("list")
+  )
+  expect_that(
+    n1$html(),
+    is_a("character")
+  )
+  #maybe use XML::htmlParse to test some things
+  expect_that(
+    doc <- XML::htmlParse(n1$html()),
+    is_a("HTMLInternalDocument")
+  )
+  expect_that(
+    XML::xpathSApply(doc,"//script",xmlValue),
+    is_a("character")  # do we test for values with regex
+  )
 })
