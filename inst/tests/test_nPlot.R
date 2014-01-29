9382573f2725cb3b5aa5349d40957167d367371c
timelyportfolio <kent.russell@timelyportfolio.com>
2014-01-28 21:53:46 -0600

    change source(.../examples.R) to test time to run

--- inst/tests/test_nPlot.R
+++ inst/tests/test_nPlot.R
@@ -57,8 +57,8 @@ test_that("nPlot output seems correct",{
   #placeholder since this will run all the examples
   #to visually inspect results
   expect_that(
-    source("../libraries/nvd3/examples.R"),
-    is_a("list") #list not important; just a way to run
+    system.time(source("../libraries/nvd3/examples.R"))[[3]] < 20,
+    is_true() # not important; just a way to run
   )
   #test other modes in show: server, inline, iframe??
 })
