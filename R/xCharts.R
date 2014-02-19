a0ba854327201d2e04545925037f253e39e06a7e
timelyportfolio <kent.russell@timelyportfolio.com>
2014-02-19 16:21:17 -0600

    continue to update xCharts with new script template and simpler xCharts.R

--- R/xCharts.R
+++ R/xCharts.R
@@ -4,18 +4,8 @@ xCharts = setRefClass('xCharts', contains = 'rCharts', methods = list(
     container <<- 'figure'
   },
   layer = function(...){
-    params_ = toXSeries(getLayer(...), series = 'main')
-    params <<- modifyList(params, Filter(Negate(is.null), params_))
-  },
-  main = function(...){
-    params$main <<- modifyList(params$main, list(...))
-  },
-  opts = function(..., replace = F){
-    params$opts <<- setSpec(params$opts, ..., replace = replace)
-  },
-  xAxis = function(..., replace = F){
-    params$xAxis <<- setSpec(params$xAxis, ..., replace = replace)
-  },
+    params <<- merge_list(getLayer(...), params)
+  },  
   getPayload = function(chartId){
     list(
       chartParams = toJSON(params[!(names(params) %in% c('type'))]), 
@@ -24,18 +14,6 @@ xCharts = setRefClass('xCharts', contains = 'rCharts', methods = list(
     )
   }
 ))
-
-toXSeries = function(params_, series = 'main'){
-  x_ = params_$x; y_ = params_$y; group = params_$group; data_ = params_$data;
-  params_$xScale = ifelse(is.numeric(params_$data[[x_]]), 'linear', 'ordinal')
-  params_$yScale = ifelse(is.numeric(params_$data[[y_]]), 'linear', 'ordinal')
-  data2 = dlply(data_, group)
-  params_[series] = list(llply(seq_along(data2), function(i){list(
-    data = fixData(data2[[i]][,c(x_, y_)]))
-  }))
-  params_$x <- params_$y <- params_$group <- params_$data <- NULL
-  return(params_)
-}
 
 xPlot <- function(x, ...){
   UseMethod('xPlot')
